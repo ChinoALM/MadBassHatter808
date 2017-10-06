5ff4af507268ded1a4420d0184151f7c660ce91e
ChinoALM <mortona35@gmail.com>
2017-10-06 11:53:28 -0400

    grammatical errors in code and comments on functions revised

--- madhat_madlib_fill_in_the_blanks.py
+++ madhat_madlib_fill_in_the_blanks.py
@@ -5,7 +5,7 @@
 #   -Once user selects a level, game displays a fill-in-the blank and a prompt for the user
 #       to fill in the first blank, and subsequently plays a full game of Madhat madlibs.
 
-# This function greets the user and prompts the user to select a level difficulty.
+# Function greets user and prompts user to select their determined level of difficulty.
 def begin_madhat_madlib():
     print '\n'+ "Welcome to the Mad Hatter's Madlib Game!" +'\n'
     print "Fill in the blanks with lyrics from me Mad Hatter's favorite Beatles' songs!" + '\n'
@@ -14,18 +14,18 @@ def begin_madhat_madlib():
     while check_input is True:
         level_input = raw_input("Type in easy, medium or hard to select a difficulty: ")
         if level_input == 'easy' or level_input == 'medium' or level_input == 'hard':
-            input_check= False
+            check_input = False
         else:
             print '\n' + 'You selected an invalid level. Please select easy, medium, or hard.' + '\n'
     return level_input
 
-# This functions returns a problem string associated with the level difficulty the user selected.
+# Function returns a problem string associated with the level difficulty the user selects.
 def madhat_problem(level,problem_dict):
     choosen_problem = problem_dict[level]
     #print choosen_problem + '\n'
     return choosen_problem
 
-# This functions returns a list of answers associated with the level difficulty the user selected.
+# Function returns a list of answers associated with the level difficulty the user selects.
 def madhat_answers(level):
     if level == 'easy':
         answer_list = ['Jude', 'bad', 'song', 'better', 'her']
@@ -35,26 +35,25 @@ def madhat_answers(level):
         answer_list =['love', 'sleeping', 'guitar', 'gently', 'weeps', 'controlled']
     return answer_list
 
-# Checks if a word in fill_in_blank is a substring of the word passed in.
+# Function checks if a word in fill_in_blank is a substring of the word passed in.
 def word_in_pos(word, fill_in_blank):
     for pos in fill_in_blank:
         if pos in word:
             return pos
     return None
 
-# Prints string that corresponds to the level of difficulty the user selected and prompts user
-# until they answer all the blanks correctly, or until the user has 3 incorrect guesses, returning
-# the number of incorrect guesses, leaving them with 1 less attempt for each incorrect answer.
+# Function prints string that corresponds to the level of difficulty the user selects and prompts user
+# until they answer all blanks correctly, or until user has 3 incorrect guesses.
 def game_instructions(string,answers,blanks,strike,string_index):
     for word in string:
         is_blank= word_in_pos(word,blanks)
         if is_blank!= None:
             while strike != 0:
                 print '\n' + " ".join(string) + '\n'
-                user_answer = raw_input("You have " + str(strike) + " attempts left. Please enter your guess: ")
+                user_answer = raw_input("You have " + str(strike) + " attempts left. Please enter your next guess: ")
                 correct = word_in_pos(user_answer,answers)
                 if correct != None:
-                    print "\n"+ "Correct!" + "\n"
+                    print "\n"+ "Congratulations, you are correct!" + "\n"
                     string[string_index] = user_answer
                     string_index = string_index+1
                     break
@@ -65,14 +64,14 @@ def game_instructions(string,answers,blanks,strike,string_index):
     return strike
     return string_one
 
-# Takes the number of incorrect guesses and tells the user if  they won or lost
+# Function uses strike_count as input to inform the user if he or she won or lost.
 def win_or_lose(strike_count):
     if strike_count == 0:
         print "\n" + "Aw, shucks. Game over, you lost! Please try again." + "\n"
     else:
-        print "\n" + "Congratulations, you won!" + "\n"
+        print "\n" + "Congratulations, you won! These are the lyrics to me Mad Hatter's favorite Beatles' songs!" + "\n"
 
-# Inputs for functions and calling upon them in a dictionary:
+# Inputs for functions, calling upon them in a dictionary:
 problem_dict ={'easy':
     '''
     Hey, ___1___, don't make it ___2___
@@ -121,5 +120,5 @@ x = game_instructions(string_one,answer_list,fill_in_blank,strike,string_index)
 
 win_or_lose(x)
 
-# This prevents the program from closing out automatically
-x = raw_input("Hit return to exit:")
+# Prevents program from closing automatically.
+x = raw_input("To exit me Mad Hatter's Madlib, please hit return:")
